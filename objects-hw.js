67c8055ae3dd08c634a8f80d6464e2e7d79220e6
Q456lm <cbbunting1@my.waketech.edu>
2025-04-15 13:09:08 -0400

    Added a little code

--- objects-hw.js
+++ objects-hw.js
@@ -5,8 +5,22 @@ const outputDiv = document.getElementById("output"); // keep this to output to p
 // 2. with the properties: difficulty, soundOn, and maxPlayers. 
 // 3. Loop through the object and 
 // 4. display the results to the page.
+let gameSettings = {
+  difficulty: "Hard",
+  soundOn: false,
+  maxPlayers: 69,
+}
 
 function problem1() {
+  result=""
+
+  for (key in gameSettings){
+    if (key == "toggleSound"){
+      break;
+    }
+    result += `${key}: `
+    result += `${gameSettings[`${key}`]}      `
+  }
   // Output results in a new result variable. use let result = ______
   outputDiv.innerHTML = result; // keep this to output to page.
 }
@@ -17,8 +31,18 @@ function problem1() {
 // 3. Call the method and output the soundOn property to the page. 
 // 4. Each time you click the button, it should alternate between true and false.
 
+gameSettings.toggleSound = function togSound(){
+  if (this.soundOn == false){
+    this.soundOn = true
+  }
+  else{
+    this.soundOn = false
+  }
+}
+
 function problem2() {
-  // Add ONE line IN THIS function that calls toggleSound method.
+  console.log(gameSettings);
+  gameSettings.toggleSound();
   outputDiv.innerHTML = `<strong>Sound is now:</strong> ${gameSettings.soundOn}`;
 }
 
@@ -27,10 +51,17 @@ function problem2() {
 // 2. with the properties flavor, size, and hasProtein (boolean). 
 // 3. Prompt the user for a size, then 
 // 4. display the updated size on the page.
+
+let smoothie = {
+  flavor: "A Minecraft Movie's Steve played by Jack Black blast",
+  size: 30,
+  hasProtein: false,
+}
   
   function problem3() {
     // keep this if else block to help with overwriting the old size. 
     // You will need to have created a newSize variable with the use of a prompt.
+    newSize = prompt(`What size do your ${smoothie.flavor}?`)
     if (newSize && newSize.trim() !== "") {
       smoothie.size = newSize.trim();
     }
@@ -46,13 +77,18 @@ function problem2() {
 // 5. displays a formatted string to the page like:
 // 6. "Name: [name], Battery Life: [batteryLife] hrs, Wireless: [true/false]".
 
+let gadget = {
+  Name: "Expolde Any Countryator",
+  batteryLife: 27,
+  Wireless: true,
+}
 
 function printGadgetSpecs(gadget) {
-  return // insert `output string here` // hint: use backticks and ${object.property} references.
+  return `Name: ${gadget.Name}, Battery Life: ${gadget.batteryLife} hrs, Wireless: ${gadget.Wireless}`
 }
 
 function problem4() {
-  outputDiv.innerHTML = `<strong>Gadget Specs:</strong> ${printGadgetSpecs(Gadget)}`;
+  outputDiv.innerHTML = `<strong>Gadget Specs:</strong> ${printGadgetSpecs(gadget)}`;
 }
 
 // Problem 5
@@ -63,12 +99,15 @@ function problem4() {
 // 5. PUSHes a new plant into the array. 
 // 6. Use the method to add two plants, then 
 // 7. loop through the object to display (list out on the page) the plants in the garden.
+let garden = {
+  plants: [],
+  addPlant: function addPlant(plant){
+    this.plants.push(plant)
+  }
+};
 
 
 function problem5() {
-  
-  // This helps with keeping your text entry box clean after every input.
-  // No need to change this, just keep the variable and object naming in mind.
   const newPlant = document.getElementById("plantInput").value;
   if (newPlant.trim() !== "") {
     garden.addPlant(newPlant);
@@ -90,23 +129,30 @@ function problem5() {
 // 3. Add a method addSong that adds a new song to the list. 
 // 4. This should take user input (prompt) to add the song to songList. 
 // 5. Then add two songs and log the playlist.
+let playlist = {
+  name: "Best songs in the known universe",
+  songList: [],
+  addSong: function newSong(song){
+    this.songList.push(song);
+  }
+}
 
 function problem6() {
   // Use a for loop that iterates 2 times to ask for two songs.
   // No need to touch the for loop except uncommenting and replacing the PLACEHOLDER.
-  // for (PLACEHOLDER) {
-  //   const song = prompt(`Enter song ${i + 1}:`);
-  //   if (song && song.trim() !== "") {
-  //     playlist.addSong(song.trim());
-  //   }
-  // }
+    for (i = 0; i<2; i++) {
+      const song = prompt(`Enter song ${i + 1}:`);
+      if (song && song.trim() !== "") {
+        playlist.addSong(song.trim());
+      }
+    }
 
 
   // This will output to the page for you:
   // Just fill in the PLACEHOLDERs with the correct info.
-  let result = `<strong>Playlist: PLACEHOLDER </strong><ul>`;
-  for (let song of PLACEHOLDER) {
-    result += `<li>${PLACEHOLDER}</li>`;
+  let result = `<strong>Playlist: ${playlist.name} </strong><ul>`;
+  for (let song of playlist.songList) {
+    result += `<li>${song}</li>`;
   }
   result += "</ul>";
   outputDiv.innerHTML = result;
